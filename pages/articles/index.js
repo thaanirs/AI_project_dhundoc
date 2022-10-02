6d30c969f8f6ecd339b44e7521685e2288f91d86
vedant-11 <v.vedantsingh.11@gmail.com>
2022-10-02 13:37:10 +0530

    added the file

---
+++ pages/articles/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "../../components/Article/Card";
+import PDFViewer from "pdf-viewer-reactjs";
+import Navbar from "../../components/Navbar";
+
+import Articles from "../../components/Article copy";
+
+const Articlepage = () => {
+  return (
+    <>
+      <Navbar />{" "}
+      <div style={{ display: "flex ", flexDirection: "row", width: "100vw" }}>
+        <div style={{ width: "80vw" }}>
+          <Articles />
+        </div>
+        <iframe
+          className="iframe"
+          src="https://nasachallenge2022.s3.ap-south-1.amazonaws.com/YWcnvauC.pdf"
+          width="100%"
+          style={{ height: "100vh" }}
+        ></iframe>
+      </div>
+    </>
+  );
+};
+
+export default Articlepage;
